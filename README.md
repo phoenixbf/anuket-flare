331e6026f21892b0f199220acda5337189aca50c
Bruno Fanini <phoenixbf@gmail.com>
2024-07-19 17:43:49 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# anuket-flare
+# Anuket
+
+This flare (plugin) adds remote control/interaction to ATON web-apps. It is being developed under [CHANGES project](https://sites.google.com/uniroma1.it/changes/home) as part of task 3.3.
+
+## Getting started
+
+1) Follow [ATON framework instructions](https://github.com/phoenixbf/aton)
+2) Just git clone or place "anuket" folder inside `/Your-ATON-folder/config/flares/` thus obtaining: `/Your-ATON-folder/config/flares/anuket/`
---
+++ public/ATON.flare.anuket.js
@@ -0,0 +1,59 @@
+/*
+    Anuket ATON flare
+
+    author: bruno.fanini_AT_gmail.com
+
+===========================================================*/
+let F = new ATON.Flare();
+
+F.setup = ()=>{
+    F._ws = undefined;
+    F._bConnected = false;
+
+    F._params = new URLSearchParams(window.location.search);
+    if (F._params.get("ak.addr")) F.connect( F._params.get("ak.addr") );
+
+    F.log("Initialized");
+};
+
+F.connect = (addr)=>{
+    F._ws = new WebSocket(addr);
+
+    F._ws.addEventListener('open', (event)=>{
+        F.log("Connected!");
+        F._bConnected = true;
+    });
+
+    F._ws.addEventListener('message', (event)=>{
+        F.onMessage(event.data);
+    });
+
+    F._ws.addEventListener('close', (event)=>{ 
+        F.log('Connection has been closed');
+        F._bConnected = false;
+    });
+
+    F._ws.addEventListener('error', (event)=>{ 
+        F.log('Error:' + event);
+        F._bConnected = false;
+    });
+};
+
+F.onMessage = (msg)=>{
+    F.console(msg);
+};
+
+F.sendMessage = (msg)=>{
+    if (!F._bConnected) return;
+
+    F._ws.send(msg);
+};
+
+
+/*
+F.update = ()=>{
+
+};
+*/
+
+F.register("anuket");
